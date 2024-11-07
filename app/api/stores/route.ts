1adfdc48b0f2aa431817e5033039eacbb4812458
abdou <abdouben-1209@hotmail.com>
2024-11-07 10:36:35 +0100

    add dynamic fetch store

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -2,6 +2,32 @@ import { auth } from "@/auth";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export async function GET(request: Request) {
+  try {
+    const session = await auth();
+    const userId = session?.user?.id;
+    const body = await request.json();
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!name) return new NextResponse("Name is required", { status: 400 });
+
+    const store = await db.store.findMany({
+      where: {
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("STORES_ROUTE", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function POST(request: Request) {
   try {
     const session = await auth();
